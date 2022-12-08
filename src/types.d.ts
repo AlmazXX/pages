b6b895632bb41f5f325078af01599b0f587aef59
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-08 14:13:55 +0600

    Added types Page, PagesList

--- src/types.d.ts
+++ src/types.d.ts
@@ -10,3 +10,12 @@ export type IPostApi = Omit<IPost, 'id'>
 export interface IPostsList {
   [id: string]: IPostApi;
 }
+
+export interface Page {
+  title: string;
+  content: string;
+}
+
+export interface PagesList {
+  [key: string]: Page;
+}
