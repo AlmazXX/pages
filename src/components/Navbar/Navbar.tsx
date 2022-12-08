c52a29428396551064abf9ff800ea85aa3c8c40e
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-08 14:21:18 +0600

    Added logic to render page links loaded from back

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,21 @@
+import { useCallback, useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import axiosApi from "../../axiosApi";
+import { PagesList } from "../../types";
 
 const Navbar = () => {
+  const [pageNames, setPageNames] = useState<string[]>([]);
+
+  const getPageNames = useCallback(async () => {
+    const { data } = await axiosApi.get<PagesList>(`/pages/.json`);
+    const pages = Object.keys(data);
+    setPageNames(pages);
+  }, []);
+
+  useEffect(() => {
+    getPageNames();
+  }, [getPageNames]);
+
   return (
     <div className="navbar navbar-expand-sm navbar-dark bg-primary">
       <div className="container">
@@ -17,16 +32,13 @@ const Navbar = () => {
                 Add
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink to="/about" className="nav-link">
-                About
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/contacts" className="nav-link">
-                Contacts
-              </NavLink>
-            </li>
+            {pageNames.map((pageName) => (
+              <li className="nav-item" key={pageName} >
+                <NavLink to={`pages/${pageName}`}className="nav-link">
+                  {pageName[0].toUpperCase() + pageName.slice(1)}
+                </NavLink>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
