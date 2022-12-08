a4a7e66d21998a04ab08ec9998c799e9324eb34c
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-08 14:22:04 +0600

    Created Pages cont Added logic to render content loaded from back

---
+++ src/containers/Pages/Pages.tsx
@@ -0,0 +1,31 @@
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axiosApi from "../../axiosApi";
+import { Page } from "../../types";
+
+const Pages = () => {
+  const { pageName } = useParams();
+  const [page, setPage] = useState<Page | null>(null);
+
+  const getPage = useCallback(async () => {
+    const { data } = await axiosApi.get<Page>(`/pages/${pageName}.json`);
+    setPage(data);
+  }, [pageName]);
+
+  useEffect(() => {
+    getPage();
+  }, [getPage]);
+
+  return (
+    <div className="row mt-3 text-center lh-lg">
+      {page && (
+        <>
+          <h4>{page.title}</h4>
+          <p>{page.content}</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Pages;
