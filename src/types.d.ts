14a75d91fe7114d299f7d6d17374fa4940aca1e9
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-09 16:20:15 +0600

    Removed unnecessary types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,16 +1,3 @@
-export interface IPost {
-  id: string;
-  title: string;
-  date: string;
-  body: string;
-}
-
-export type IPostApi = Omit<IPost, 'id'>
-
-export interface IPostsList {
-  [id: string]: IPostApi;
-}
-
 export interface Page {
   title: string;
   content: string;
