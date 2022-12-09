5f811ff20644c0f677c21e201e705257fdcb318d
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-09 16:16:20 +0600

    Added logic to load new links with urlPath change Removed unnecessary links Added capitalize func

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,22 @@
 import { useCallback, useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import axiosApi from "../../axiosApi";
+import { capitalize } from "../../helpers";
 import { PagesList } from "../../types";
 
 const Navbar = () => {
+  const { pathname } = useLocation();
   const [pageNames, setPageNames] = useState<string[]>([]);
 
   const getPageNames = useCallback(async () => {
-    const { data } = await axiosApi.get<PagesList>(`/pages/.json`);
-    const pages = Object.keys(data);
-    setPageNames(pages);
+      const { data } = await axiosApi.get<PagesList>(`/pages/.json`);
+      const pages = Object.keys(data);
+      setPageNames(pages);
   }, []);
 
   useEffect(() => {
-    getPageNames();
-  }, [getPageNames]);
+    getPageNames().catch(console.error);
+  }, [getPageNames, pathname]);
 
   return (
     <div className="navbar navbar-expand-sm navbar-dark bg-primary">
@@ -22,26 +24,21 @@ const Navbar = () => {
         <span className="navbar-brand">Blog</span>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <NavLink to="/" className="nav-link">
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/new-post" className="nav-link">
-                Add
-              </NavLink>
-            </li>
             {pageNames.map((pageName) => (
-              <li className="nav-item" key={pageName} >
-                <NavLink to={`pages/${pageName}`}className="nav-link">
-                  {pageName[0].toUpperCase() + pageName.slice(1)}
+              <li className="nav-item" key={pageName}>
+                <NavLink to={`pages/${pageName}`} className="nav-link">
+                  {capitalize(pageName)}
                 </NavLink>
               </li>
             ))}
             <li className="nav-item">
               <NavLink to="/pages/admin" className="nav-link">
                 Admin
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/pages/create" className="nav-link">
+                Create page
               </NavLink>
             </li>
           </ul>
